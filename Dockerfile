475fb0513d5858ec9982422424680c566933c186
adamhenson <ahenson@paperlesspost.com>
2019-09-25 14:17:27 -0400

    Revert "chore: lighthouse-check add all the params."
    This reverts commit cb0ed63728be27afd175f42ef8c357dac7280aa4.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
 FROM foosoftware/lighthouse-check:latest
 COPY entrypoint.sh /entrypoint.sh
-
-RUN mkdir /tmp/artifacts
-
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,3 @@
 #!/bin/sh -l
 
-lighthouse-check \
-  --author $1 \
-  --apiToken $2 \
-  --awsAccessKeyId $3 \
-  --awsBucket $4 \
-  --awsRegion $5\
-  --awsSecretAccessKey $6 \
-  --branch $7 \
-  --configFile $8 \
-  --emulatedFormFactor $9 \
-  --locale $10 \
-  --help $11 \
-  --outputDirectory /tmp/artifacts \
-  --pr $12 \
-  --sha $13 \
-  --slackWebhookUrl $14 \
-  --tag $15 \
-  --timeout $16 \
-  --throttling $17 \
-  --throttlingMethod $18 \
-  --urls $19 \
-  --verbose $20 \
-  --wait $21 \
+lighthouse-check --urls $1
