81a3c5441b398311a83c86d00a1da98f6f61dd67
adamhenson <ahenson@paperlesspost.com>
2019-09-25 10:09:39 -0400

    chore: initial testing.

--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -l
-
-lighthouse-check --urls $1
---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { lighthouseCheck } = require('@foo-software/lighthouse-check');
+
+(async () => {
+  try {
+    const urls = core.getInput('urls');
+    console.log('github', github.context);
+    await lighthouseCheck({
+      urls: urls.join()
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
+
