45a38127ab913402228c669a527eb38348fb34d4
adamhenson <ahenson@paperlesspost.com>
2019-09-26 12:33:36 -0400

    chore: test minimum scores.

--- index.js
+++ index.js
@@ -17,12 +17,75 @@ const normalizeInput = input => {
   return input;
 };
 
+const getScoreFailMessage = ({
+  name,
+  url,
+  minScore,
+  score
+}) => {
+  // if inputs are not specified - assume we shouldn't fail
+  if (typeof minScore !== 'number' || typeof score !== 'number') {
+    return [];
+  }
+
+  if (score < minScore) {
+    return [`${url}: ${name}: minimum score: ${minScore}, actual score: ${score}`];
+  }
+
+  return [];
+};
+
+const getFailureMessages = ({
+  minAccessibilityScore,
+  minBestPracticesScore,
+  minPerformanceScore,
+  minProgressiveWebAppScore,
+  minSeoScore,
+  results
+}) => {
+  return results.data.reduce((accumulator, current => ([
+    ...(getScoreFailMessage({
+      name: 'Accessibility',
+      minScore: minAccessibilityScore,
+      score: current.scores.accessibility,
+      ...current
+    })),
+    ...(getScoreFailMessage({
+      name: 'Best Practices',
+      minScore: minBestPracticesScore,
+      score: current.scores.bestPractices,
+      ...current
+    })),
+    ...(getScoreFailMessage({
+      name: 'Performance',
+      minScore: minPerformanceScore,
+      score: current.scores.performance,
+      ...current
+    })),
+    ...(getScoreFailMessage({
+      name: 'Progressive Web App',
+      minScore: minProgressiveWebAppScore,
+      score: current.scores.progressiveWebApp,
+      ...current
+    })),
+    ...(getScoreFailMessage({
+      name: 'SEO',
+      minScore: minSeoScore,
+      score: current.scores.seo,
+      ...current
+    }))
+  ]), []));
+}
+
 (async () => {
   try {
     const urls = normalizeInput(core.getInput('urls'));
 
     const minAccessibilityScore = normalizeInput(core.getInput('minAccessibilityScore'));
-    console.log('type', typeof minAccessibilityScore);
+    const minBestPracticesScore = normalizeInput(core.getInput('minBestPracticesScore'));
+    const minPerformanceScore = normalizeInput(core.getInput('minPerformanceScore'));
+    const minProgressiveWebAppScore = normalizeInput(core.getInput('minProgressiveWebAppScore'));
+    const minSeoScore = normalizeInput(core.getInput('minSeoScore'));
 
     const results = await lighthouseCheck({
       author: normalizeInput(core.getInput('author')),
@@ -48,6 +111,26 @@ const normalizeInput = input => {
       verbose: normalizeInput(core.getInput('verbose')),
       wait: normalizeInput(core.getInput('wait'))
     });
+
+    // if we need to fail when scores are too low...
+    if (minAccessibilityScore || minBestPracticesScore
+      || minPerformanceScore || minProgressiveWebAppScore || minSeoScore) {
+      const failures = getFailureMessages({
+        minAccessibilityScore,
+        minBestPracticesScore,
+        minPerformanceScore,
+        minProgressiveWebAppScore,
+        minSeoScore,
+        results
+      });
+
+      // if we have scores that were below the minimum requirement
+      if (failures.length) {
+        // comma-separate error messages and remove the last comma
+        const failureMessage = failures.join().slice(0, -1);
+        throw new Error(`Minimum score requirements failed. ${failureMessage}`);
+      }
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
