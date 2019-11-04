0b12f37528c954ca8fec4b51d8f14090cf855af8
Adam Henson <adamhenson1979@gmail.com>
2019-11-03 23:42:02 -0500

    feat: lo.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const formatInput = input => {
 (async () => {
   try {
     const urls = formatInput(core.getInput('urls'));
-    console.log('comments_url', get(github, 'context.payload.pull_request.comments_url'));
+    console.log('comments_url', get(github, 'context.payload.pull_request'));
 
     const results = await lighthouseCheck({
       author: formatInput(core.getInput('author')),
