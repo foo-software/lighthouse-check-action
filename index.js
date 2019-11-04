fe4e0aa9c32e179c1f30e090fa96fe00d3218228
Adam Henson <adamhenson1979@gmail.com>
2019-11-04 00:10:33 -0500

    feat: log.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const formatInput = input => {
     const urls = formatInput(core.getInput('urls'));
     const apiPrUrl = get(github, 'context.payload.repository.pulls_url');
 
-    console.log('hmmm', formatInput(core.getInput('prCommentAccessToken'));
+    console.log('hmmm', formatInput(core.getInput('prCommentAccessToken')));
 
     const results = await lighthouseCheck({
       author: formatInput(core.getInput('author')),
