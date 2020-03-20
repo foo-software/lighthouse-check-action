5f699316fcc7029b0062aba74200530d8df5661f
Adam Henson <adamhenson1979@gmail.com>
2020-03-19 20:15:46 -0400

    test: extra header upload artifacts.

--- test/headerServer.js
+++ test/headerServer.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => res.json(req.headers));
+app.get('/', (req, res) => res.send(`
+<!DOCTYPE html>
+  <html>
+  <head>
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+  </head>
+  <body>
+    <h1>
+      req.headers['x-hello-world'] = ${req.headers['x-hello-world'] || 'undefined'}
+    </h1>
+  </body>
+</html>
+`));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
