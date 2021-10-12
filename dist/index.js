7dc5ba059dc0815718e00372a92875453830a9b8
adamhenson <adamhenson1979@gmail.com>
2021-10-11 22:31:58 -0400

    fix: @vercel/ncc is causing a bug

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,30 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __importDefault(require("@actions/core"));
+const core = __importStar(require("@actions/core"));
 const lodash_get_1 = __importDefault(require("lodash.get"));
-const github_1 = __importDefault(require("@actions/github"));
+const github = __importStar(require("@actions/github"));
 const lighthouse_check_1 = require("@foo-software/lighthouse-check");
 const formatInput = (input) => {
     if (input === 'true') {
@@ -21,46 +40,46 @@ const formatInput = (input) => {
 };
 (async () => {
     try {
-        const urls = formatInput(core_1.default.getInput('urls'));
-        const extraHeaders = core_1.default.getInput('extraHeaders');
-        const commentUrl = core_1.default.getInput('commentUrl');
-        const prApiUrl = (0, lodash_get_1.default)(github_1.default, 'context.payload.pull_request.url');
+        const urls = formatInput(core.getInput('urls'));
+        const extraHeaders = core.getInput('extraHeaders');
+        const commentUrl = core.getInput('commentUrl');
+        const prApiUrl = (0, lodash_get_1.default)(github, 'context.payload.pull_request.url');
         const results = await (0, lighthouse_check_1.lighthouseCheck)({
-            author: formatInput(core_1.default.getInput('author')),
-            apiToken: formatInput(core_1.default.getInput('apiToken')),
-            awsAccessKeyId: formatInput(core_1.default.getInput('awsAccessKeyId')),
-            awsBucket: formatInput(core_1.default.getInput('awsBucket')),
-            awsRegion: formatInput(core_1.default.getInput('awsRegion')),
-            awsSecretAccessKey: formatInput(core_1.default.getInput('awsSecretAccessKey')),
-            branch: formatInput(core_1.default.getInput('branch')),
-            configFile: formatInput(core_1.default.getInput('configFile')),
-            emulatedFormFactor: formatInput(core_1.default.getInput('emulatedFormFactor')),
+            author: formatInput(core.getInput('author')),
+            apiToken: formatInput(core.getInput('apiToken')),
+            awsAccessKeyId: formatInput(core.getInput('awsAccessKeyId')),
+            awsBucket: formatInput(core.getInput('awsBucket')),
+            awsRegion: formatInput(core.getInput('awsRegion')),
+            awsSecretAccessKey: formatInput(core.getInput('awsSecretAccessKey')),
+            branch: formatInput(core.getInput('branch')),
+            configFile: formatInput(core.getInput('configFile')),
+            emulatedFormFactor: formatInput(core.getInput('emulatedFormFactor')),
             extraHeaders: !extraHeaders ? undefined : JSON.parse(extraHeaders),
-            locale: formatInput(core_1.default.getInput('locale')),
-            help: formatInput(core_1.default.getInput('help')),
-            outputDirectory: formatInput(core_1.default.getInput('outputDirectory')),
-            overridesJsonFile: formatInput(core_1.default.getInput('overridesJsonFile')),
-            pr: formatInput(core_1.default.getInput('pr')),
-            prCommentAccessToken: formatInput(core_1.default.getInput('accessToken')),
-            prCommentEnabled: formatInput(core_1.default.getInput('prCommentEnabled')),
-            prCommentSaveOld: formatInput(core_1.default.getInput('prCommentSaveOld')),
+            locale: formatInput(core.getInput('locale')),
+            help: formatInput(core.getInput('help')),
+            outputDirectory: formatInput(core.getInput('outputDirectory')),
+            overridesJsonFile: formatInput(core.getInput('overridesJsonFile')),
+            pr: formatInput(core.getInput('pr')),
+            prCommentAccessToken: formatInput(core.getInput('accessToken')),
+            prCommentEnabled: formatInput(core.getInput('prCommentEnabled')),
+            prCommentSaveOld: formatInput(core.getInput('prCommentSaveOld')),
             prCommentUrl: commentUrl || (!prApiUrl ? undefined : `${prApiUrl}/reviews`),
-            sha: formatInput(core_1.default.getInput('sha')),
-            slackWebhookUrl: formatInput(core_1.default.getInput('slackWebhookUrl')),
-            tag: formatInput(core_1.default.getInput('tag')),
-            timeout: formatInput(core_1.default.getInput('timeout')),
-            throttling: formatInput(core_1.default.getInput('throttling')),
-            throttlingMethod: formatInput(core_1.default.getInput('throttlingMethod')),
+            sha: formatInput(core.getInput('sha')),
+            slackWebhookUrl: formatInput(core.getInput('slackWebhookUrl')),
+            tag: formatInput(core.getInput('tag')),
+            timeout: formatInput(core.getInput('timeout')),
+            throttling: formatInput(core.getInput('throttling')),
+            throttlingMethod: formatInput(core.getInput('throttlingMethod')),
             urls: typeof urls !== 'string' ? undefined : urls.split(','),
-            verbose: formatInput(core_1.default.getInput('verbose')),
-            wait: formatInput(core_1.default.getInput('wait')),
+            verbose: formatInput(core.getInput('verbose')),
+            wait: formatInput(core.getInput('wait')),
             isGitHubAction: true
         });
-        core_1.default.setOutput('lighthouseCheckResults', JSON.stringify(results));
+        core.setOutput('lighthouseCheckResults', JSON.stringify(results));
     }
     catch (error) {
         if (error instanceof Error) {
-            core_1.default.setFailed(error.message);
+            core.setFailed(error.message);
         }
     }
 })();
